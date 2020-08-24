a28907a331812def6ad2e6a71b5631abf31b1a47
Jeremia Verhulst <jeremiaverhulst@mbp-van-jeremia.home>
2020-08-24 16:30:43 +0200

    Added password reset functionality and logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -28,9 +29,11 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/logout", name="app_logout")
+     * @return RedirectResponse
      */
-    public function logout()
+    public function logout(): RedirectResponse
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->redirectToRoute('app_login');
+       // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
