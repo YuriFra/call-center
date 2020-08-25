c82635e818ef52fab3c141a96594b7bdf6639f53
becode <yf_be@yahoo.co.uk>
2020-08-25 09:08:41 +0200

    update composer/config

--- config/bundles.php
+++ config/bundles.php
@@ -13,9 +13,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-<<<<<<< HEAD
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-=======
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
->>>>>>> d948d78fd2c8ad1cf9deeda4ac0b7eb9d1b71ed6
 ];
